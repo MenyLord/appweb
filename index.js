f93dbd8961acc552592035b5fc43dddf279e4bf3
Meny <jesusmeny2912@gmail.com>
2025-05-22 13:25:46 -0600

    agregar el endpoint de velocidad

--- index.js
+++ index.js
@@ -6,10 +6,14 @@ const PORT=process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+app.get("/velocidad",(req, res)=>{
+res.json({nombre: "Jesus", Apellido: "Gonzalez" });
+});
+
 app.get("/temperatura",(req, res)=>{
 res.json({valor: "10°C", timestamp: new Date().toISOString() });
-})
+});
 
 app.listen(PORT, ()=>{
     console.log(`servidor corriendo en puerto ${PORT}`);
-})
+});
